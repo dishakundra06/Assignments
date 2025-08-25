ae8bc00ee2417c8b4b152678550eb0443ab6766a
dishakundra06 <ddisha_be24@thapar.edu>
2025-08-25 22:15:07 +0530

    Create Q4.cpp

---
+++ ASSIGNMENT 1-102030180/Q4.cpp
@@ -0,0 +1,84 @@
+//reverse element of an array
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Reversed array: ";
+    for (int i = n - 1; i >= 0; i--)
+        cout << arr[i] << " ";
+}
+//Matrix multiplication
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10][10], b[10][10], c[10][10];
+    int r1, c1, r2, c2;
+
+    cout << "Enter rows and columns of first matrix: ";
+    cin >> r1 >> c1;
+    cout << "Enter rows and columns of second matrix: ";
+    cin >> r2 >> c2;
+
+    if (c1 != r2) {
+        cout << "Matrix multiplication not possible!\n";
+        return 0;
+    }
+
+    cout << "Enter first matrix:\n";
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c1; j++)
+            cin >> a[i][j];
+
+    cout << "Enter second matrix:\n";
+    for (int i = 0; i < r2; i++)
+        for (int j = 0; j < c2; j++)
+            cin >> b[i][j];
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < c1; k++)
+                c[i][j] += a[i][k] * b[k][j];
+        }
+    }
+
+    cout << "Resultant matrix:\n";
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c2; j++)
+            cout << c[i][j] << " ";
+        cout << endl;
+    }
+}
+//the Transpose of a Matrix
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10][10], transpose[10][10], r, c;
+    cout << "Enter rows and columns: ";
+    cin >> r >> c;
+
+    cout << "Enter matrix elements:\n";
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            cin >> a[i][j];
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            transpose[j][i] = a[i][j];
+
+    cout << "Transpose:\n";
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++)
+            cout << transpose[i][j] << " ";
+        cout << endl;
+    }
+}
