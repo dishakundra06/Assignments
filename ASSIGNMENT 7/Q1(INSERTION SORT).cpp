817cf1167b219ac689e41f93a75620f3d1dfa5c2
dishakundra06 <ddisha_be24@thapar.edu>
2025-11-20 21:55:35 +0530

    Create Q1(INSERTION SORT).cpp

---
+++ ASSIGNMENT 7/Q1(INSERTION SORT).cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertionSort(vector<int> &a) {
+    int n = a.size();
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    if (!(cin >> n)) return 0;
+    vector<int> a(n);
+    cout << "Enter elements:\n";
+    for (int i = 0; i < n; i++) cin >> a[i];
+    insertionSort(a);
+    cout << "Sorted array:\n";
+    for (int x : a) cout << x << " ";
+    cout << "\n";
+    return 0;
+}
